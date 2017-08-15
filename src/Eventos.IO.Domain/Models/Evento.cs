88d1f9cb6981b703d3a19f22069ecf510aeb75a0
Rômulo Martins <rm_romulomartins@hotmail.com>
2017-08-15 03:09:36 -0300

    Iniciando o projeto

---
+++ src/Eventos.IO.Domain/Models/Evento.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Eventos.IO.Domain.Models
+{
+    using Eventos.IO.Domain.Core.Models;
+    using FluentValidation;
+    using System;    
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Evento : Entity<Evento>
+    {
+        public Evento(string nome, 
+                      DateTime dataInicio, 
+                      DateTime dataFim, 
+                      bool gratuito, 
+                      decimal valor, 
+                      bool online,
+                      string nomeEmpresa)
+        {
+            Nome = nome;
+            DataInicio = dataInicio;
+            DataFim = dataFim;
+            Gratuito = gratuito;
+            Valor = valor;
+            Online = online;
+            NomeEmpresa = nomeEmpresa;
+        }
+        public string Nome { get; private set; }
+        public string DescricaoCurta { get; private set; }
+        public string DescricaoLonga { get; private set; }
+        public DateTime DataInicio { get; private set; }
+        public DateTime DataFim { get; private set; }
+        public bool Gratuito { get; private set; }
+        public decimal Valor { get; private set; }
+        public bool Online { get; private set; }
+        public string NomeEmpresa { get; private set; }
+        public Catgoria Categoria { get; private set; }
+        public ICollection<Tag> Tags { get; private set; }
+        public Endereco Endereco { get; private set; }
+        public Organizador Organizador { get; private set; }
+
+        public override bool EhValido()
+        {
+            Validar();
+            return ValidationResult.IsValid;
+        }
+
+        #region Validações
+
+        private void Validar()
+        {
+            ValidarNome();
+            ValidarValor();
+            ValidarData();
+            ValidarLocal();
+            ValidarNomeEmpresa();
+            ValidationResult = Validate(this);
+        }
+
+        private void ValidarNome()
+        {
+            RuleFor(c => c.Nome)
+                .NotEmpty().WithMessage("O nome do evento precisa ser fornecido.")
+                .Length(2, 150).WithMessage("O nome do evento precisa ter entre 2 e 150 caracteres.");
+        }
+
+        private void ValidarValor()
+        {
+            if (!Gratuito)
+                RuleFor(c => c.Valor)
+                    .ExclusiveBetween(1, 10000)
+                    .WithMessage("O valor deve estar entre 1.00 e 10.000.");
+
+            if (Gratuito)
+                RuleFor(c => c.Valor)
+                    .ExclusiveBetween(0, 0).When(e => e.Gratuito)
+                    .WithMessage("O valor não deve ser diferente de 0 para um evento gratuito.");
+        }
+
+        private void ValidarData()
+        {
+            RuleFor(c => c.DataInicio)
+                .GreaterThan(c => c.DataFim)
+                .WithMessage("A data de início deve ser maior que a data do final do evento.");
+
+            RuleFor(c => c.DataInicio)
+                .LessThan(DateTime.Now)
+                .WithMessage("A data de início nõ deve ser menor que a data atual.");
+        }
+
+        private void ValidarLocal()
+        {
+            if (Online)
+                RuleFor(c => c.Endereco)
+                    .Null().When(c => c.Online)
+                    .WithMessage("O evento não deve possuir um endereço se for online.");
+
+            if (!Online)
+                RuleFor(c => c.Endereco)
+                    .NotNull().When(c => c.Online == false)
+                    .WithMessage("O evento deve possuir um endereço.");
+
+        }
+
+        private void ValidarNomeEmpresa()
+        {
+            RuleFor(c => c.NomeEmpresa)
+                .NotEmpty().WithMessage("O nome do organizador precisa ser fornecido.")
+                .Length(2, 150).WithMessage("O nome do organizador precisa ter entre 2 e 150 caracteres.");
+        }
+
+        #endregion
+
+    }
+}
